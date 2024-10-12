1c85ccbd5e8e97aace4aa48538434692d831950d
xmyLydia <836728902crystal@gmail.com>
2024-10-12 23:16:55 +0300

    Initialize

---
+++ src/test/java/com/schedular/taskscheduler/SchedulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedular.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

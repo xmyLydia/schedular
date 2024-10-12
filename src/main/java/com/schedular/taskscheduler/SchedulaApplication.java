105d30ec789ecbef22774fdc00d98eb6ef941ccd
xmyLydia <836728902crystal@gmail.com>
2024-10-12 23:16:55 +0300

    Initialize

---
+++ src/main/java/com/schedular/taskscheduler/SchedulaApplication.java
@@ -0,0 +1,13 @@
+package com.schedular.taskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulaApplication.class, args);
+    }
+
+}
